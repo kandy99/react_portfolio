183892d839b82bc3efeb42fef916ed333c1f4897
dobal19 <dobalsachin97@gmail.com>
2020-04-12 12:28:11 +0530

    Updates

---
+++ precache-manifest.0a7caecc3ed27c49c2443a9d9880af07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7f783e5ca76aa3637646b6f39287dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42d2e7672154cfa69457",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "9d770d48c10e45b059be",
+    "url": "/static/css/main.8011637f.chunk.css"
+  },
+  {
+    "revision": "42d2e7672154cfa69457",
+    "url": "/static/js/2.1ad2aeb7.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.1ad2aeb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d770d48c10e45b059be",
+    "url": "/static/js/main.78db70fb.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  }
+]);
